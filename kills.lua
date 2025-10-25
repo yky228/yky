4261eddb2d218ea3e457dc3d31db0ac0bb203a88
yky228 <knoxlua0@gmail.com>
2025-10-24 19:41:13 -0700

    Update kills.lua

--- kills.lua
+++ kills.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/2b9327fd18bda2bcbb1af387f4a3f8d3"))()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/91b89fe87c2cc58e7296a48a28c4f02c"))()
